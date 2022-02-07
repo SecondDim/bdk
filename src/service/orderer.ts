181811a31b270d8a575fb8aa7d4de183f634a848
kth-tw <88102592+kth-tw@users.noreply.github.com>
2022-02-07 18:05:17 +0800

    rename orderer step orgConfigComputeUpdateConfigTx and orgConfigComputeUpdateConfigTx to computeUpdateConfigTx

--- src/service/orderer.ts
+++ src/service/orderer.ts
@@ -181,7 +181,7 @@ export default class Orderer extends AbstractService {
     logger.debug(`Org Orderer Add Org: add ${dto.orgName} in ${dto.channelName}`)
 
     await this.addOrgToChannelSteps().fetchChannelConfig(dto)
-    await this.addOrgToChannelSteps().orgConfigComputeUpdateConfigTx(dto)
+    await this.addOrgToChannelSteps().computeUpdateConfigTx(dto)
   }
 
   /**
@@ -193,7 +193,7 @@ export default class Orderer extends AbstractService {
         logger.debug('add org to channel step1 (fetchChannelConfig)')
         return await (new Channel(this.config, this.infra)).fetchChannelConfig(dto.channelName, this.config.orgType, dto.orderer)
       },
-      orgConfigComputeUpdateConfigTx: async (dto: OrdererAddOrgToChannelType) => {
+      computeUpdateConfigTx: async (dto: OrdererAddOrgToChannelType) => {
         logger.debug('add org to channel step2 (orgConfigComputeUpdateAndSignConfigTx)')
         const { channelName, orgName } = dto
 
@@ -223,7 +223,7 @@ export default class Orderer extends AbstractService {
     logger.debug(`Org Orderer Add Consenter: add ${dto.hostname} of ${dto.orgName} in ${dto.channelName}`)
 
     await this.addConsenterToChannelSteps().fetchChannelConfig(dto)
-    await this.addConsenterToChannelSteps().hostnameComputeUpdateConfigTx(dto)
+    await this.addConsenterToChannelSteps().computeUpdateConfigTx(dto)
   }
 
   /**
@@ -235,7 +235,7 @@ export default class Orderer extends AbstractService {
         logger.debug('add consenter to channel step1 (fetchChannelConfig)')
         return await (new Channel(this.config, this.infra)).fetchChannelConfig(dto.channelName, this.config.orgType, dto.orderer)
       },
-      hostnameComputeUpdateConfigTx: async (dto: OrdererAddConsenterToChannelType) => {
+      computeUpdateConfigTx: async (dto: OrdererAddConsenterToChannelType) => {
         logger.debug('add consenter to channel step2 (hostnameComputeUpdateAndSignConfigTx)')
         const { orderer, channelName } = dto
 
